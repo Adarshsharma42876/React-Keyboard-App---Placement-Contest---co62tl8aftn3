ea57f56ba4017899cc9f3865ac13c8ce380745b0
ADARSH SHARMA <120009672+Adarshsharma42876@users.noreply.github.com>
2023-05-28 16:06:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,44 +4,32 @@ import React, { useState, useEffect } from "react";
 const keys = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
 
 const App = () => {
-    const [input, setInput] = useState("");
-    const [quote, setQuote] = useState(null);
-    function handleClick(key) {
-        console.log("Clicked!");
-        setInput((state) => state + key);
+  const [input,setInput] = useState("");
+  const [quote,setQuote] = useState("");
+
+  useEffect(()=>{
+    if (input === "forty two") {
+      fetch("https://api.quotable.io/random")
+      .then(res=>res.json())
+      .then(data=>setQuote(data.content))
+    }else{
+      setQuote("");
     }
-    useEffect(() => {
-        if (input === "forty two") {
-            (async () => {
-                const query = await fetch("https://api.quotable.io/random");
-                const response = await query.json();
-                setQuote(response.content);
-            })();
-        }
-    }, [input]);
-    return (
-        <>
-            {!quote ? (
-                <div className="keyboard">
-                    <div className="preview">{input}</div>
-                    <div>
-                        {keys.map((key) => (
-                            <button
-                                key={key}
-                                value={input}
-                                onClick={() => handleClick(key)}
-                                id={key === " " ? `key-space` : `key-${key}`}
-                            >
-                                {key === " " ? "Space" : key.toUpperCase()}
-                            </button>
-                        ))}
-                    </div>
-                </div>
-            ) : (
-                <div className="quote">{quote}</div>
-            )}
-        </>
-    );
+  },[input])
+
+  return (
+    <div className="keyboard">
+      <div className="preview">{input}</div>
+      {!!quote && <div className="quote">{quote}</div>}
+      <div>
+        {keys.map((key) => (
+          <button key={key} id={key === " " ? `key-space` : `key-${key}`} onClick={()=>{setInput(prev=>prev + key)}}>
+            {key === " " ? "Space" : key.toUpperCase()}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
